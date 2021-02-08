8d4a60d31f18c40db5cd2f70ef21ddf007969c1a
figaro27 <sweetfigaro27@gmail.com>
2021-02-08 11:31:03 +0900

    changed gitignore for module

---
+++ config.js
@@ -0,0 +1,15 @@
+var config = {}
+
+config.mysql = {
+	host: "localhost",
+	user: "user",
+	password: "barcaNo.10messi",
+	database: "heat-rps"
+}
+
+config.mainAccount = '13731872534881421597'
+config.secret = 'region ill rate first invest one inch dumb burst address fit warrior'
+
+config.fee = 10  // 10%
+
+module.exports.config = config;
